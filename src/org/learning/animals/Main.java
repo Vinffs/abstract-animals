bdc9cd89c3e324f7e45c2a4a14ee552388982928
Vinffs <vin.demiranda@gmail.com>
2024-04-04 14:48:02 +0200

    used methods makeSwim and makeFly

--- src/org/learning/animals/Main.java
+++ src/org/learning/animals/Main.java
@@ -1,6 +1,14 @@
 package org.learning.animals;
 
 public class Main {
+
+    public static void makeFly(CanFly animal) {
+        animal.fly();
+    }
+
+    public static void makeSwim(CanSwim animal) {
+        animal.swim();
+    }
     public static void main(String[] args) {
 
         System.out.println("New Dog");
@@ -16,6 +24,7 @@ public class Main {
 
         System.out.println("New Dolphin");
         Dolphin bob = new Dolphin();
+        makeSwim(bob);
         bob.sound();
         bob.eat();
         bob.sleep();
@@ -26,6 +35,7 @@ public class Main {
 
         System.out.println("New Hawk");
         Hawk murica = new Hawk();
+        makeFly(murica);
         murica.sound();
         murica.eat();
         murica.sleep();
@@ -37,6 +47,7 @@ public class Main {
 
         System.out.println("New Sparrow");
         Sparrow jack = new Sparrow();
+        makeFly(jack);
         jack.sound();
         jack.eat();
         jack.sleep();
