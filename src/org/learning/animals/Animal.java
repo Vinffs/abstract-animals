b94992a2f1ecf6e2bd827eb63bcfbb61cb728929
Vinffs <vin.demiranda@gmail.com>
2024-04-04 14:27:01 +0200

    created abstract class Animal that has method sleep (same for all) and abstract methods for sound and eat (each different depending by the animal)

---
+++ src/org/learning/animals/Animal.java
@@ -0,0 +1,12 @@
+package org.learning.animals;
+
+abstract class Animal {
+    void sleep() {
+        System.out.println("Zzz");
+    }
+
+    abstract void sound();
+
+    abstract void eat();
+
+}
